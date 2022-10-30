80ccb1e3edfb3b230fbed4550f543e08cb2b2ae8
“XBlib” <2712137464@qq.com>
2022-10-31 01:02:57 +0800

    好的

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,8 +1,115 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+    <el-container style="min-height: 100vh; ">
+      <el-aside :width="sideWidth + 'px'" style="background-color: rgb(238, 241, 246); overflow-x: hidden; box-shadow:rgba(0,21,41,0.35) 2px 0 6px; ">
+        <el-menu :default-openeds="['1', '3']" style="min-height: 100%; overflow-x: hidden;"
+                 background-color="rgb(48,65,86)"
+                 text-color="#fff"
+                 :collapse-transition="false"
+                 :collapse="isCollapse"
+        >
+          <div style="height: 60px; line-height: 60px; text-align: center">
+            <img src="../assets/logo.png" alt="" style="width: 20px; position: relative; top: 5px; margin-right: 5px; ">
+            <b style="color: aliceblue" v-show="logoTextShow">后台管理系统</b>
+          </div>
+          <el-submenu index="1">
+            <template slot="title">
+              <i class="el-icon-message"></i>
+              <span slot="title">导航一</span>
+            </template>
+            <el-menu-item-group title="分组2">
+              <el-menu-item index="1-3">选项3</el-menu-item>
+            </el-menu-item-group>
+            <el-submenu index="1-4">
+              <template slot="title">选项4</template>
+              <el-menu-item index="1-4-1">选项4-1</el-menu-item>
+            </el-submenu>
+          </el-submenu>
+          <el-submenu index="2">
+            <template slot="title"><i class="el-icon-menu"></i>
+              <span slot="title">导航二</span>
+            </template>
+            <el-submenu index="2-4">
+              <template slot="title">选项4</template>
+              <el-menu-item index="2-4-1">选项4-1</el-menu-item>
+            </el-submenu>
+          </el-submenu>
+          <el-submenu index="3">
+            <template slot="title"><i class="el-icon-setting"></i>
+              <span slot="title">导航三</span>
+            </template>
+            <el-submenu index="3-4">
+              <template slot="title">选项4</template>
+              <el-menu-item index="3-4-1">选项4-1</el-menu-item>
+            </el-submenu>
+          </el-submenu>
+        </el-menu>
+      </el-aside>
+      <el-container>
+
+        <el-header style=" font-size: 12px; border: 1px solid #ccc; line-height: 60px; display: flex">
+          <div style="flex: 1; font-size: 20px">
+            <span :class="collapseBtnClass" style="cursor: pointer" @click="collapse"></span>
+          </div>
+          <el-dropdown style="width: 70px; cursor: pointer">
+            <span>God</span><i class="el-icon-arrow-down" style="margin-left: 5px"></i>
+            <el-dropdown-menu slot="dropdown">
+              <el-dropdown-item>个人信息</el-dropdown-item>
+              <el-dropdown-item>退出</el-dropdown-item>
+            </el-dropdown-menu>
+          </el-dropdown>
+        </el-header>
+
+        <el-main>
+
+          <div style="margin-bottom: 30px;">
+            <el-breadcrumb separator="/">
+              <el-breadcrumb-item :to="{path: '/'}">首页</el-breadcrumb-item>
+              <el-breadcrumb-item>用户管理</el-breadcrumb-item>
+            </el-breadcrumb>
+            </div>
+
+          <div style="padding: 10px 0">
+            <el-input style="width: 200px" placeholder="请输入名称" suffix-icon="el-icon-search" v-model="username">
+            </el-input>
+            <el-button class="ml-5" type="primary" @click="load">搜索</el-button>
+
+            <el-input style="width: 200px;margin-left: 30px" placeholder="请输入电话" suffix-icon="el-icon-search" v-model="phone">
+            </el-input>
+            <el-button class="ml-5" type="primary" @click="load">搜索</el-button>
+          </div>
+          <div style="margin: 10px 0">
+            <el-button type="primary">新增 <i class="el-icon-circle-plus-outline"></i></el-button>
+            <el-button type="primary">批量删除 <i class="el-icon-remove-outline"></i></el-button>
+            <el-button type="primary">导入 <i class="el-icon-bottom"></i> </el-button>
+            <el-button type="primary">导出 <i class="el-icon-top"></i></el-button>
+          </div>
+          <el-table :data="tableData" border stripe :header-cell-class-name="headerBg">
+            <el-table-column prop="username" label="姓名" width="140" align="center"></el-table-column>
+            <el-table-column prop="nickname" label="昵称" width="120" align="center"></el-table-column>
+            <el-table-column prop="email" label="邮箱" align="center"></el-table-column>
+            <el-table-column prop="phone" label="电话" align="center"></el-table-column>
+            <el-table-column prop="address" label="地址" align="center"></el-table-column>
+            <el-table-column label="操作" align="center">
+              <template slot-scope="scope">
+                <el-button type="success">编辑 <i class="el-icon-edit"></i></el-button>
+                <el-button type="danger">删除 <i class="el-icon-delete"></i></el-button>
+              </template>
+            </el-table-column>
+          </el-table>
+          <div class="block" style="padding: 10px 0">
+            <el-pagination
+                @size-change="handleSizeChange"
+                @current-change="handleCurrentChange"
+                :current-page.sync="pageNum"
+                :page-sizes="[5, 10, 15, 20]"
+                :page-size="pageSize"
+                layout="total, sizes, prev, pager, next, jumper"
+                :total="total">
+            </el-pagination>
+          </div>
+        </el-main>
+      </el-container>
+    </el-container>
 </template>
 
 <script>
@@ -11,8 +118,62 @@ import HelloWorld from '@/components/HelloWorld.vue'
 
 export default {
   name: 'HomeView',
-  components: {
-    HelloWorld
+  data() {
+    return {
+      tableData: [],
+      total:0,
+      username:"",
+      phone:"",
+      pageNum:1,
+      pageSize:10,
+      collapseBtnClass: 'el-icon-s-fold',
+      isCollapse: false,
+      sideWidth: 200,
+      logoTextShow: true,
+      headerBg: 'headerBg'
+    }
+  },
+  created() {
+    //请求分页查询数据
+    this.load()
+  },
+  methods: {
+    collapse() { //点击收缩按钮触发
+      this.isCollapse = !this.isCollapse
+      if(this.isCollapse) { //收缩
+        this.sideWidth = 64
+        this.collapseBtnClass = 'el-icon-s-unfold'
+        this.logoTextShow = false
+      } else {
+        this.sideWidth = 200
+        this.collapseBtnClass = 'el-icon-s-fold'
+        this.logoTextShow = true
+      }
+    },
+
+    load() {
+      fetch("http://localhost:9090/user/page?pageNum=" + this.pageNum + "&pageSize=" + this.pageSize + "&username=" + this.username + "&phone=" + this.phone  )
+          .then(res => res.json()).then(res => {
+        this.tableData = res.data
+        console.log(res)
+        this.total = res.total
+      })
+    },
+
+    handleSizeChange(pageSize) {
+      this.pageSize = pageSize
+      this.load()
+    },
+    handleCurrentChange(pageNum) {
+      this.pageNum = pageNum
+      this.load()
+    }
   }
 }
 </script>
+
+<style>
+.headerBg {
+  background: #dedede !important;
+}
+</style>
